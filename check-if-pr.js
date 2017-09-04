e090c616ec8580fc98f3ec03a7aff41d57be4613
Marks Polakovs <marks@markspolakovs.me>
2017-09-05 01:52:29 +0200

    Better checks

--- check-if-pr.js
+++ check-if-pr.js
@@ -1,4 +1,4 @@
-if (process.env.TRAVIS_PULL_REQUEST !== "false" && process.env.TRAVIS_PULL_REQUEST_SLUG.indexOf("VolunteerLiveTeam") !== -1) {
+if (process.env.TRAVIS_PULL_REQUEST !== "false" && !(process.env.REDDIT_USERNAME)) {
     console.log(`*CANNOT CONTINUE BUILD*
 For security reasons, we cannot run builds on PRs from forks.
 Please wait for a maintainer to merge it into dev and build it from there`);
