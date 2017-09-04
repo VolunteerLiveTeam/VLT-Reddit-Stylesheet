47f2bf19e13774c893ee0e9a9fcfdc15ac904c1d
Marks Polakovs <marks@markspolakovs.me>
2017-09-04 21:04:24 +0200

    Better error handling

--- screenshots.js
+++ screenshots.js
@@ -1,40 +1,45 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 const PAGES = require("./screenshotPages.json");
 
-const before = process.argv.indexOf('before') !== -1;
+const before = process.argv.indexOf("before") !== -1;
 
 (async () => {
-    try {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.setViewport({ width: 1920, height: 1800 });
-        
-        await page.goto("https://reddit.com/login");
-        await page.focus("#user_login");
-        await page.type(process.env.REDDIT_USERNAME);
-        await page.focus("#passwd_login");
-        await page.type(process.env.REDDIT_PASSWORD);
-        await page.click(`#login-form button`);
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1800 });
 
-        await page.waitForNavigation();
-        console.log('Logged in.');
+  await page.goto("https://reddit.com/login");
+  await page.focus("#user_login");
+  await page.type(process.env.REDDIT_USERNAME);
+  await page.focus("#passwd_login");
+  await page.type(process.env.REDDIT_PASSWORD);
+  await page.click(`#login-form button`);
 
-        try {
-            await page.click(".cookie-infobar button");
-        } catch (e) {
-            console.error(`Couldn't click cookie button. Big deal.`);
-        }
+  await page.waitForNavigation();
+  console.log("Logged in.");
 
-        for (const key of Object.keys(PAGES)) {
-            const url = PAGES[key];
-            await page.goto(url);
-            await page.screenshot({ path: `screenshot-${before ? 'before' : 'after'}-${key}.png` });
-        }
-        console.log('Done.');
-        browser.close();
-        process.exit(0);
-    } catch (e) {
-        console.error(e);
-    }
-})();
+  try {
+    await page.click(".cookie-infobar button");
+  } catch (e) {
+    console.error(`Couldn't click cookie button. Big deal.`);
+  }
+
+  for (const key of Object.keys(PAGES)) {
+    const url = PAGES[key];
+    await page.goto(url);
+    await page.screenshot({
+      path: `screenshot-${before ? "before" : "after"}-${key}.png`
+    });
+  }
+  console.log("Done.");
+  browser.close();
+})().then(
+  res => {
+    process.exit(0);
+  },
+  err => {
+    console.error(`ERROR! ${err}`);
+    process.exit(1);
+  }
+);
