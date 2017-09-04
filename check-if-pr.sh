26be81f4ee8677ebcab159f84a30b1b98b69f6a8
Marks Polakovs <marks@markspolakovs.me>
2017-09-04 21:42:30 +0200

    OOPS

--- check-if-pr.sh
+++ check-if-pr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_PULL_REQUEST" == "true" ] && [ "$TRAVIS_PULL_REQUEST_SLUG" != *"VolunteerLiveTeam"* ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ "$TRAVIS_PULL_REQUEST_SLUG" != *"VolunteerLiveTeam"* ]; then
     echo "*CANNOT CONTINUE BUILD*"
     echo "For security reasons, we cannot run builds on PRs from forks."
     echo "Please wait for a maintainer to merge your PR into dev, then they'll build it from dev."
